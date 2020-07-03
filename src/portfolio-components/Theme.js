c475d9e98966c3a53fb2d2e6da910f3b847cf2f2
yuliacech <yulia-ichin-norbu@ya.ru>
2020-07-03 21:40:41 +0200

    Install and configure gatsby

--- src/portfolio-components/Theme.js
+++ src/portfolio-components/Theme.js
@@ -3,7 +3,9 @@ import React from "react";
 class Theme extends React.Component {
   render() {
     for (let [paletteProperty, palettePropertyValue] of Object.entries(this.props.palette)) {
-      document.documentElement.style.setProperty(paletteProperty, palettePropertyValue);
+      if (typeof document !== `undefined`) {
+        document.documentElement.style.setProperty(paletteProperty, palettePropertyValue);
+      }
     }
     return null;
   }
